b589f1ab35fed7fd9651df747f1211bfc1a24176
kakiuchi yusuke <ka030709@gmail.com>
2019-05-29 15:47:55 +0900

    キャッシュテスト

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,11 +1,27 @@
-// service-worker.js
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
+var CACHE_NAME  = "MKit-cache-v1";
+var urlsToCache = [
+    "../img/icon/"
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(
+            function(cache){
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(
+        function (response) {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
 });
-
-// 現状では、この処理を書かないとService Workerが有効と判定されないようです
-self.addEventListener('fetch', function(event) {});
